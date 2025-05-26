51179e27d7cdd46d1ca72573aad68cf52943ef76
Ilya Archakov <lancher88@gmail.com>
2025-05-26 22:04:51 +0300

    v 1.2

--- delivery_service.py
+++ delivery_service.py
@@ -1,18 +1,36 @@
 def min_platforms(robots: list[int], limit: int) -> int:
-    robots.sort()
-    left, right = 0, len(robots) - 1
+
+    '''
+    Вычисляет минимальное количество платформ,
+    необходимых для транспортировки роботов.
+
+    Args:
+        robots: принимает на вход массив целых чисел в первой строке
+                через пробел — это вес отдельных роботов.
+
+        limit:  принимает предельную грузоподъёмность платформы
+                во второй строке.
+    Returns:
+        Целое число, указывающее на необходимое количество платформ
+        для транспортировки.
+
+    ID посылки: 138810434
+    '''
+
+    sorted_robots = sorted(robots)
+    light_idx, heavy_idx = 0, len(robots) - 1
     platforms = 0
 
-    while left <= right:
-        if robots[left] + robots[right] <= limit:
-            left += 1
-        right -= 1
+    while light_idx <= heavy_idx:
+        if sorted_robots[light_idx] + sorted_robots[heavy_idx] <= limit:
+            light_idx += 1
+        heavy_idx -= 1
         platforms += 1
 
     return platforms
 
 
 if __name__ == '__main__':
-    robots = list(map(int, input().split()))
-    limit = int(input())
-    print(min_platforms(robots, limit))
+    print(min_platforms([int(inputed_numbers) for
+                         inputed_numbers in input().split()],
+                        int(input())))
